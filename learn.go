be65da74dbfb7ea885fc1e478c197cfe1ea730d2
miiniper <miiniper@gmail.com>
2020-11-03 20:18:19 +0800

    1

---
+++ learn.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+func typeof(v interface{}) string {
+	fmt.Println("#########This is reflect result########################")
+	return reflect.TypeOf(v).String()
+}
+
+type item struct {
+	Name string
+}
+
+func (i item) String() string {
+	return fmt.Sprintf("1item name: %v", i.Name)
+}
+
+type person struct {
+	Name string
+	Sex  string
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("2person name: %v sex: %v", p.Name, p.Sex)
+}
+
+func Parse(i interface{}) interface{} {
+	fmt.Println(typeof(i))
+	switch i.(type) {
+	case string:
+		return &item{
+			Name: i.(string),
+		}
+	case []string:
+		data := i.([]string)
+		length := len(data)
+		if length == 2 {
+			return &person{
+				Name: data[0],
+				Sex:  data[1],
+			}
+		} else {
+			return nil
+		}
+	default:
+		panic(errors.New("type match miss"))
+	}
+	return nil
+}
+
+func main() {
+	p1 := Parse("Apple").(*item)
+	fmt.Println(p1)
+	p2 := Parse([]string{"zhangsan", "man"}).(*person)
+	fmt.Println(p2)
+}
